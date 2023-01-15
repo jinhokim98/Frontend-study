a3ba754f402ff781648bf4e2cc69613e7422485b
김진호 <rlawlsgh1227@gmail.com>
2023-01-15 12:39:52 +0900

    2023.01.15: study JavaScript class, object, array api

---
+++ JavaScript/js/array.js
@@ -0,0 +1,91 @@
+'use strict';
+
+// Array
+
+// 1. Declearation
+const array = new Array();
+const array2 = [1, 2];
+
+// 2. Index position
+const fruits = ['🍎', '🍇'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[fruits.length - 1]);
+
+console.clear();
+// 3. Looping over an array
+// print all fruits
+
+// a. for
+for (let i = 0; i<fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// b. for of
+for(let fruit of fruits) {
+    console.log(fruit);
+}
+
+// c. forEach
+fruits.forEach((fruit) => console.log(fruit));
+
+// 4. Addition, deletion, copy
+// push: add an item to the end
+
+fruits.push('🍏', '🍓');
+console.log(fruits);
+
+// pop: remove an item from the end
+fruits.pop();
+fruits.pop();
+console.log(fruits);
+
+// unshift: add an item to the beginning
+fruits.unshift('🍏', '🍓');
+console.log(fruits);
+
+// shift: remove an item from the beginning
+fruits.shift();
+fruits.shift();
+console.log(fruits);
+
+// note!! shift, unshift are slower than pop, push
+
+console.clear();
+// splice: remove an item by index position
+fruits.push('🍏', '🍓', '🍒', '🍑');
+console.log(fruits);
+
+fruits.splice(1, 1);
+console.log(fruits);
+
+fruits.splice(1, 1, '🍍', '🍌');
+console.log(fruits);
+
+// combine two arrays
+const fruits2 = ['🍉', '🍈'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
+
+// 5. Searching
+// find the index
+console.clear();
+console.log(fruits);
+console.log(fruits.indexOf('🍓'));
+console.log(fruits.indexOf('🍒'));
+console.log(fruits.indexOf('🍈'));
+
+
+// includes
+console.log(fruits.includes('🍒'));
+console.log(fruits.includes('🍈'));
+
+// lastIndexof
+console.clear();
+fruits.push('🍎');
+console.log(fruits);
+console.log(fruits.indexOf('🍎'));
+console.log(fruits.lastIndexOf('🍎'));
